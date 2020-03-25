9b641ea1148460f2f25b7bb56e4681ee1aa0b661
Christian Pardo <chrispardopersonal@gmail.com>
2020-03-25 21:26:20 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -3,7 +3,7 @@ def unsafe?(speed)
     return true
   elsif speed < 40
     return true
-    elsif speed=35 > 40 && speed=90  < 60
+    else speed=35 > 40 && speed=90  < 60
     return false
   end
 end
